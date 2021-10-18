8490bc8dc4f1c0f39980c7928d60f3dfcc6bb34e
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-18 09:49:38 -0400

    structure components in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './header/Header';
-
+import './App.scss';
+import SearchResult from './searchResults/SearchResult';
+import Recipe from './recipe/Recipe';
 const App = () => {
   return (
     <div className='container'>
       <Header />
+      <SearchResult />
+      <Recipe />
     </div>
   );
 };
