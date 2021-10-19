d1c20ee99ec9b8a332e3c1e1c21d049c36d06600
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-19 11:08:04 -0400

    import React-Query/DevTool

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './header/Header';
 import './App.scss';
 import SearchResult from './searchResults/SearchResult';
 import Recipe from './recipe/Recipe';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
+  const [recipeName, setRecipeName] = useState('');
+  const [recipeData, setRecipeData] = useState([]);
+
   return (
-    <>
-      <Header />
-      <SearchResult />
-      <Recipe />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Header recipeName={recipeName} setRecipeName={setRecipeName} />
+      <SearchResult
+        recipeName={recipeName}
+        recipeData={recipeData}
+        setRecipeData={setRecipeData}
+      />
+      <Recipe recipe={recipeData} />
+      {/* <ReactQueryDevtools /> */}
+    </QueryClientProvider>
   );
 };
 
