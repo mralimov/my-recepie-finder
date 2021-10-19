f59bd69ce196f15e59ea32f1ea5c5dac894b6f38
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-19 11:06:13 -0400

    update Header comp

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,12 +2,12 @@ import React, { Fragment } from 'react';
 import Form from './Form';
 import Nav from './Nav';
 
-const Header = () => {
+const Header = ({ recipeName, setRecipeName }) => {
   return (
     <Fragment>
       <header className='header'>
         <img src='../img/logo.png' alt='Logo' className='header__logo' />
-        <Form />
+        <Form recipeName={recipeName} setRecipeName={setRecipeName} />
         <Nav />
       </header>
     </Fragment>
