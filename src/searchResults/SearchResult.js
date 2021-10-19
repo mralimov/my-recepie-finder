0f3c513fc32f38ec5237ce9d99113c130f4ea0ca
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-19 18:03:07 -0400

    update SearchResult comp

--- src/searchResults/SearchResult.js
+++ src/searchResults/SearchResult.js
@@ -4,53 +4,69 @@ import Loader from '../Loader';
 import RecipeList from './RecipeList';
 
 const SearchResult = ({ recipeName, recipeData, setRecipeData }) => {
+  const [currentPage, setCurrentPage] = useState(0);
   const KEY = 'ffa960fd-e870-4fb9-b79d-f2d19c62463c';
+
+  // const getTenRecipies = (data, page) => {
+  //   const start = (page - 1) * 10;
+  //   const end = page * 10;
+  //   return setRecipeData(data.slice(start, end));
+  // };
+  let start = (currentPage - 1) * 10;
+  let end = currentPage * 10;
 
   const { get, loading } = useFetch(
     `https://forkify-api.herokuapp.com/api/v2/recipes?`
   );
+
   useEffect(() => {
     get(`search=${recipeName}&key=${KEY}`)
-      .then((data) => setRecipeData(data.data.recipes))
+      .then((data) => {
+        setRecipeData([data.data.recipes]);
+        console.log(data.data.recipes);
+      })
       .catch((err) => console.log(err));
-  }, [recipeName]);
+  }, [recipeName, currentPage]);
 
-  console.log(recipeData);
+  // if (!recipeData) return;
+  // console.log(recipeData);
   return (
     <div className='search-results'>
       <ul className='results'>
         {loading && <Loader />}
-        {/* <!--  ffa960fd-e870-4fb9-b79d-f2d19c62463c
-       
-           --> */}
-        {recipeData.map((recipe) => {
+        {/* {recipeData.map((recipe) => {
           return <RecipeList key={recipe.id} recipe={recipe} />;
-        })}
+        })} */}
+        {<RecipeList recipes={recipeData} currentPage={currentPage} />}
       </ul>
 
       <div className='pagination'>
-        {/* <button className="btn--inline pagination__btn--prev">
-            <svg className="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-left"></use>
+        {currentPage > 0 && (
+          <button
+            className='btn--inline pagination__btn--prev'
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            <svg className='search__icon'>
+              <use href='src/img/icons.svg#icon-arrow-left'></use>
             </svg>
-            <span>Page 1</span>
+            <span>Page {currentPage}</span>
           </button>
-          <button className="btn--inline pagination__btn--next">
-            <span>Page 3</span>
-            <svg className="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
-            </svg>
-          </button> */}
+        )}
+        <button
+          className='btn--inline pagination__btn--next'
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          <span>Page {currentPage + 2}</span>
+          <svg className='search__icon'>
+            <use href='src/img/icons.svg#icon-arrow-right'></use>
+          </svg>
+        </button>
       </div>
 
       <p className='copyright'>
         &copy; Copyright by
-        <a
-          className='twitter-link'
-          target='_blank'
-          href='https://twitter.com/jonasschmedtman'
-        >
-          Jonas Schmedtmann
+        <a className='twitter-link' target='_blank' href='#'>
+          Donnie Alimov{' '}
         </a>
         . Use for learning or your portfolio. Don't use to teach. Don't claim as
         your own.
