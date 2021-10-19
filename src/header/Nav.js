474d733ca727a64cdbc271f90eea4538bfd48617
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-19 11:05:43 -0400

    update Nav comp

--- src/header/Nav.js
+++ src/header/Nav.js
@@ -2,36 +2,35 @@ import React, { Fragment } from 'react';
 
 const Nav = () => {
   return (
-    <Fragment>
-      <nav className='nav'>
-        <ul className='nav__list'>
-          <li className='nav__item'>
-            <button className='nav__btn nav__btn--add-recipe'>
-              <svg className='nav__icon'>
-                <use href='../img/icons.svg#icon-edit'></use>
-              </svg>
-              <span>Add recipe</span>
-            </button>
-          </li>
-          <li className='nav__item'>
-            <button className='nav__btn nav__btn--bookmarks'>
-              <svg className='nav__icon'>
-                <use href='../img/icons.svg#icon-bookmark'></use>
-              </svg>
-              <span>Bookmarks</span>
-            </button>
-            <div className='bookmarks'>
-              <ul className='bookmarks__list'>
-                <div className='message'>
-                  <div>
-                    <svg>
-                      <use href='../img/icons.svg#icon-smile'></use>
-                    </svg>
-                  </div>
-                  <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
+    <nav className='nav'>
+      <ul className='nav__list'>
+        <li className='nav__item'>
+          <button className='nav__btn nav__btn--add-recipe'>
+            <svg className='nav__icon'>
+              <use href='../img/icons.svg#icon-edit'></use>
+            </svg>
+            <span>Add recipe</span>
+          </button>
+        </li>
+        <li className='nav__item'>
+          <button className='nav__btn nav__btn--bookmarks'>
+            <svg className='nav__icon'>
+              <use href='../img/icons.svg#icon-bookmark'></use>
+            </svg>
+            <span>Bookmarks</span>
+          </button>
+          <div className='bookmarks'>
+            <ul className='bookmarks__list'>
+              <div className='message'>
+                <div>
+                  <svg>
+                    <use href='../img/icons.svg#icon-smile'></use>
+                  </svg>
                 </div>
+                <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
+              </div>
 
-                {/* <li className="preview">
+              {/* <li className="preview">
                     <a className="preview__link" href="#23456">
                       <figure className="preview__fig">
                         <img src="src/img/test-1.jpg" alt="Test" />
@@ -44,12 +43,11 @@ const Nav = () => {
                       </div>
                     </a>
                   </li>  */}
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </Fragment>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
