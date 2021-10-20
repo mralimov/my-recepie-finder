27ff3e8e0d144443728d4a872848b7451e75b017
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-20 13:02:50 -0400

    cleaning App Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import Header from './header/Header';
 import './App.scss';
 import SearchResult from './searchResults/SearchResult';
 import Recipe from './recipe/Recipe';
-import { ReactQueryDevtools } from 'react-query/devtools';
-
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient();
 
 const App = () => {
   const [recipeName, setRecipeName] = useState('');
   const [recipeData, setRecipeData] = useState([]);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Header recipeName={recipeName} setRecipeName={setRecipeName} />
       <SearchResult
         recipeName={recipeName}
@@ -22,8 +17,7 @@ const App = () => {
         setRecipeData={setRecipeData}
       />
       <Recipe recipe={recipeData} />
-      {/* <ReactQueryDevtools /> */}
-    </QueryClientProvider>
+    </>
   );
 };
 
