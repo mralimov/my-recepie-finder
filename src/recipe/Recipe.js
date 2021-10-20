8d7056e8ab2c4b1e58e840f91dd3003141aca285
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-20 14:50:29 -0400

    Recipe component

--- src/recipe/Recipe.js
+++ src/recipe/Recipe.js
@@ -1,128 +1,144 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useFetch from '../useFetch/UseFetch';
+import { BASE_URL, KEY } from '../config';
+import Icons from '../img/icons.svg';
+import RecipeIngredients from './RecipeIngredients';
 
-const Recipe = ({ recipe }) => {
+const Recipe = ({ currentRecipe }) => {
+  const { get, loading } = useFetch(BASE_URL);
+  const [recipeData, setRecipeData] = useState([]);
+  const [recipeClicked, setRecipeClicked] = useState(false);
+
+  useEffect(() => {
+    get(`/${currentRecipe}?key=${KEY}`)
+      .then((data) => {
+        console.log(data.data.recipe);
+        const { recipe } = data.data;
+        setRecipeData(recipe);
+        setRecipeClicked(true);
+      })
+      .catch((err) => console.log(err));
+  }, [currentRecipe]);
+
+  const {
+    id,
+    image_url,
+    cooking_time,
+    ingredients,
+    publisher,
+    servings,
+    source_url,
+    title,
+  } = recipeData;
+
+  console.log(id, image_url);
   return (
-    <div className='recipe'>
-      <div className='message'>
-        <div>
-          <svg>
-            <use href='src/img/icons.svg#icon-smile'></use>
-          </svg>
-        </div>
-        <p>Start by searching for a recipe or an ingredient. Have fun!</p>
-      </div>
-      {/* 
+    <>
+      <div className='recipe'>
+        {!recipeClicked && (
+          <div className='message'>
+            <div>
+              <svg>
+                <use href={Icons + '#icon-smile'}></use>
+              </svg>
+            </div>
+            <p>Start by searching for a recipe or an ingredient. Have fun!</p>
+          </div>
+        )}
+        {/* 
         <!-- <div class="spinner">
           <svg>
-            <use href="src/img/icons.svg#icon-loader"></use>
+            <use href={Icons + "#icon-loader"></use>}
           </svg>
         </div> -->
 
         <!-- <div class="error">
             <div>
               <svg>
-                <use href="src/img/icons.svg#icon-alert-triangle"></use>
+                <use href={Icons + "#icon-alert-triangle}"></use>
               </svg>
             </div>
             <p>No recipes found for your query. Please try again!</p>
-          </div> -->
+      </div> -->*/}
 
-        <!--
-        <figure class="recipe__fig">
-          <img src="src/img/test-1.jpg" alt="Tomato" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>Pasta with tomato cream sauce</span>
-          </h1>
-        </figure>
-
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">45</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">4</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+        {recipeClicked && (
+          <>
+            <figure className='recipe__fig'>
+              <img src={image_url} alt='Tomato' className='recipe__img' />
+              <h1 className='recipe__title'>
+                <span>{title}</span>
+              </h1>
+            </figure>
+            <div className='recipe__details'>
+              <div className='recipe__info'>
+                <svg className='recipe__info-icon'>
+                  <use href={Icons + '#icon-clock'}></use>
                 </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
+                <span className='recipe__info-data recipe__info-data--minutes'>
+                  {cooking_time}
+                </span>
+                <span className='recipe__info-text'>{cooking_time}</span>
+              </div>
+              <div className='recipe__info'>
+                <svg className='recipe__info-icon'>
+                  <use href={Icons + '#icon-users'}></use>
+                </svg>
+                <span className='recipe__info-data recipe__info-data--people'>
+                  {servings}
+                </span>
+                <span className='recipe__info-text'>{servings}</span>
+
+                <div className='recipe__info-buttons'>
+                  <button className='btn--tiny btn--increase-servings'>
+                    <svg>
+                      <use href={Icons + '#icon-minus-circle'}></use>
+                    </svg>
+                  </button>
+                  <button className='btn--tiny btn--increase-servings'>
+                    <svg>
+                      <use href={Icons + '#icon-plus-circle'}></use>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <div className='recipe__user-generated'>
                 <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                  <use href={Icons + '#icon-user'}></use>
+                </svg>
+              </div>
+              <button className='btn--round'>
+                <svg className=''>
+                  <use href={Icons + '#icon-bookmark-fill'}></use>
                 </svg>
               </button>
             </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">1000</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">g</span>
-                pasta
-              </div>
-            </li>
-
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">0.5</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">cup</span>
-                ricotta cheese
-              </div>
-            </li>
-          </ul>
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>
-        --> */}
-    </div>
+            {ingredients &&
+              ingredients.map((ingredient) => {
+                <RecipeIngredients key={title} ingredient={ingredient} />;
+              })}
+            <div className='recipe__directions'>
+              <h2 className='heading--2'>How to cook it</h2>
+              <p className='recipe__directions-text'>
+                This recipe was carefully designed and tested by
+                <span className='recipe__publisher'>{publisher}</span>. Please
+                check out directions at their website.
+              </p>
+              <a
+                className='btn--small recipe__btn'
+                href={source_url}
+                target='_blank'
+              >
+                <span>Directions</span>
+                <svg className='search__icon'>
+                  <use href={Icons + '#icon-arrow-right'}></use>
+                </svg>
+              </a>
+            </div>{' '}
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
