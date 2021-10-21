3308398d6f03917497dc5ee58d7e4aa45bd682d8
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-21 08:35:52 -0400

    rendering ingredients

--- src/recipe/RecipeIngredients.js
+++ src/recipe/RecipeIngredients.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import Icons from '../img/icons.svg';
-const RecipeIngredients = ({ ingredient }) => {
-  const { quantity, unit, description } = ingredient;
+const RecipeIngredients = ({ ingredients, key }) => {
   return (
     <div class='recipe__ingredients'>
       <h2 class='heading--2'>Recipe ingredients</h2>
       <ul class='recipe__ingredient-list'>
-        <li class='recipe__ingredient'>
-          <svg class='recipe__icon'>
-            <use href={Icons + '#icon-check'}></use>
-          </svg>
-          <div class='recipe__quantity'>{quantity}</div>
-          <div class='recipe__description'>
-            <span class='recipe__unit'>{unit}</span>
-            {description}
-          </div>
-        </li>
+        {ingredients.map((ingredient) => {
+          const { quantity, unit, description } = ingredient;
+
+          return (
+            <li key={key} class='recipe__ingredient'>
+              <svg class='recipe__icon'>
+                <use href={Icons + '#icon-check'}></use>
+              </svg>
+              <div class='recipe__quantity'>{quantity}</div>
+              <div class='recipe__description'>
+                <span class='recipe__unit'>{unit}</span>
+                {description}
+              </div>
+            </li>
+          );
+        })}
 
         {/* <li class='recipe__ingredient'>
             <svg class='recipe__icon'>
