c2a02d4887829619de7700b46099189bfda44dee
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-10-27 14:18:01 -0400

    bookmark function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { useState } from 'react';
-import Header from './header/Header';
+import Header from './components/header/Header';
 import './App.scss';
-import SearchResult from './searchResults/SearchResult';
-import Recipe from './recipe/Recipe';
+import SearchResult from './components/searchResults/SearchResult';
+import Recipe from './components/recipe/Recipe';
 
 const App = () => {
   const [recipeName, setRecipeName] = useState('');
   const [recipeData, setRecipeData] = useState([]);
   const [currentRecipe, setCurrentRecipe] = useState('');
+  const [bookmark, setBookmark] = useState([]);
+
+  console.log(bookmark);
+
   return (
     <>
-      <Header recipeName={recipeName} setRecipeName={setRecipeName} />
+      <Header
+        recipeData={recipeData}
+        bookmark={bookmark}
+        recipeName={recipeName}
+        setRecipeName={setRecipeName}
+      />
       <SearchResult
         recipeName={recipeName}
         recipeData={recipeData}
         setRecipeData={setRecipeData}
         setCurrentRecipe={setCurrentRecipe}
       />
-      <Recipe recipe={recipeData} currentRecipe={currentRecipe} />
+      <Recipe
+        recipe={recipeData}
+        currentRecipe={currentRecipe}
+        setBookmark={setBookmark}
+      />
     </>
   );
 };
