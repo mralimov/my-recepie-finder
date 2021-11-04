c08adfca36684dfd1e077789ebc33f9442070d3a
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-11-03 23:54:28 -0400

    BookmarkRecipe component

---
+++ src/components/header/BookmarkedRecipes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const BookmarkedRecipes = ({ bookmarkedRecipe }) => {
+  const { image_url, title, publisher } = bookmarkedRecipe;
+  return (
+    <>
+      <li className='preview'>
+        <a className='preview__link' href='#23456'>
+          <figure className='preview__fig'>
+            <img src={image_url} alt={title} />
+          </figure>
+          <div className='preview__data'>
+            <h4 className='preview__name'>{title}</h4>
+            <p className='preview__author'>{publisher}</p>
+          </div>
+        </a>
+      </li>
+    </>
+  );
+};
+
+export default BookmarkedRecipes;
