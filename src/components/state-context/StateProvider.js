e9cca8c493f0b6245efab4a27742a5ff5c182a56
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-11-16 18:48:37 -0500

    refactor state provider

--- src/components/state-context/StateProvider.js
+++ src/components/state-context/StateProvider.js
@@ -2,13 +2,29 @@ import React from 'react';
 import StateContext from './state-context';
 
 const StateProvider = (props) => {
-  const addBookmarkHandler = () => {};
+  const [allRecipes, setAllRecipes] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
 
-  const getAllRecipesHandler = (recipeId) => {};
+  const bookmarkHandler = (currentRecipe) => {
+    let checkRecipe =
+      bookmark.filter((item) => item.id === currentRecipe).length <= 0;
+
+    let bookmarkData = recipe.filter((item) => item.id === currentRecipe);
+
+    if (checkRecipe) {
+      setBookmark((prevData) => [...prevData, ...bookmarkData]);
+    } else {
+      let filterBookmark = bookmark.filter((item) => item.id !== currentRecipe);
+
+      setBookmark([...filterBookmark]);
+    }
+  };
   const stateContext = {
-    allRecipes: [],
-    bookmarks: [],
-    currentRecipeId: '',
+    allRecipes: allRecipes,
+    setAllRecipes: setAllRecipes,
+    bookmarks: bookmarks,
+    setBookmarks: setBookmarks,
+    toggleBookmark: bookmarkHandler,
     inputtedRecipeName: '',
   };
   return (
