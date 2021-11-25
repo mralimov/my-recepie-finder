4e4e06625ffca9d4c41d5ddd0a82feb1a590a6cb
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-11-25 13:59:05 -0500

    NoBookmarks comp

---
+++ src/components/header/NoBookmarks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NoBookmarks = () => {
+  return (
+    <div className='message'>
+      <div>
+        {/* <svg>
+                <use href={Icons + '#icon-smile'}></use>
+              </svg> */}
+      </div>
+      <p>No bookmarks yet. Find a nice recipe and bookmark it!!!</p>
+    </div>
+  );
+};
+
+export default NoBookmarks;
