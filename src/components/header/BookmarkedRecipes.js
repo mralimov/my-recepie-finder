25994bfbec877587e380549c85fe72b3d313ae0f
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2021-11-25 13:58:37 -0500

    bookmarkrecipies comp

--- src/components/header/BookmarkedRecipes.js
+++ src/components/header/BookmarkedRecipes.js
@@ -4,19 +4,17 @@ const BookmarkedRecipes = ({ bookmarkedRecipe }) => {
   const { image_url, title, publisher } = bookmarkedRecipe;
 
   return (
-    <>
-      <li className='preview'>
-        <a className='preview__link' href='#23456'>
-          <figure className='preview__fig'>
-            <img src={image_url} alt={title} />
-          </figure>
-          <div className='preview__data'>
-            <h4 className='preview__name'>{title}</h4>
-            <p className='preview__author'>{publisher}</p>
-          </div>
-        </a>
-      </li>
-    </>
+    <li className='preview'>
+      <a className='preview__link' href='#23456'>
+        <figure className='preview__fig'>
+          <img src={image_url} alt={title} />
+        </figure>
+        <div className='preview__data'>
+          <h4 className='preview__name'>{title}</h4>
+          <p className='preview__author'>{publisher}</p>
+        </div>
+      </a>
+    </li>
   );
 };
 
